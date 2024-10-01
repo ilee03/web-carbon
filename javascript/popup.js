8fe4a405005c8f8bf33dac7c2f582caf2f7b11d3
ilee03 <149883974+ilee03@users.noreply.github.com>
2024-09-30 21:25:12 -0400

    d

--- javascript/popup.js
+++ javascript/popup.js
@@ -22,64 +22,7 @@ function formatCarbonWeight(value) {
 	return value + suffix;
 }
 
-function renderPage() {
-	var today = bg.getDayCount(0)
-	var todayCarbon = document.getElementById('today-carbon');
-	todayCarbon.innerHTML = formatCarbonWeight(today * carbonPerPage);
 
-	var dayArr = [];
-	var chart = document.getElementById('chart');
-	for (var i = 29; i >= 0; i--) {
-		var dayCount = bg.getDayCount(i);
-		dayArr[i] = dayCount * carbonPerPage;
-	}
-	var max = dayArr.length ? Math.max.apply(null, dayArr) : 0;
-
-	var columnHeight = 120.0;
-	var ratio = columnHeight / max;
-	var bar = null;
-	var sum = 0;
-	var days = 0;
-	chart.style.height = (parseInt(columnHeight)) + 'px';
-
-	for (var i = 29; i >= 0; i--) {
-		var dayCount = dayArr[i];
-		var barHeight = parseInt(dayCount * ratio);
-
-		var column = document.createElement('div');
-		column.setAttribute('class', 'chart-column');
-		column.setAttribute('title', formatCarbonWeight(dayCount) + ' on ' + bg.formatDate(i, '-'));
-		column.style.height = parseInt(columnHeight) + 'px';
-
-		var area = document.createElement('div');
-		area.setAttribute('class', 'chart-area');
-		area.style.height = parseInt(columnHeight) + 'px';
-		column.appendChild(area);
-
-		var barWrap = document.createElement('div');
-		barWrap.setAttribute('class', 'chart-bar-wrap');
-		barWrap.style.height = barHeight + 'px';
-		area.appendChild(barWrap);
-
-		bar = document.createElement('div');
-		bar.setAttribute('class', 'chart-bar');
-		if (i == 0) {
-			bar.setAttribute('class', 'chart-bar today');
-		}
-		bar.style.height = barHeight + 'px';
-		barWrap.appendChild(bar);
-		chart.appendChild(column);
-
-		if (dayCount > 0) {
-			sum += dayCount;
-			days++;
-		}
-	}
-
-	if (days > 7) {
-		var chartDefault = document.getElementById('chart-default');
-		chartDefault.setAttribute('class', 'chart-default-hidden');
-	}
 
 	var avgDay = sum / days;
 	if (avgDay) {
@@ -96,4 +39,4 @@ function renderPage() {
 			ft.innerHTML = "🌴  You need to plant " + (year / trees).toFixed(0) + " trees to offset this amount.";
 		}
 	}
-}
+
